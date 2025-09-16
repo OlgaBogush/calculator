8c2fc900b57f2166438e4bd58b05c14e0c46338f
OlgaBogush <bohush.olga@gmail.com>
2025-09-16 12:23:06 +0300

    feat: add class Calculator

---
+++ calculator/app.js
@@ -0,0 +1,69 @@
+class Calculator {
+  constructor(prevElement, currentElement) {
+    this.prevElement = prevElement
+    this.currentElement = currentElement
+
+    this.clear()
+  }
+
+  clear() {
+    this._prevValue = ""
+    this._currentValue = ""
+    this._operation = undefined
+  }
+
+  appendNumber(number) {
+    this._currentValue += number
+  }
+
+  getDisplayNumber(number) {
+    return number
+  }
+
+  updateDisplay() {
+    this.currentElement.innerText = this.getDisplayNumber(this._currentValue)
+
+    if (this._operation !== undefined) {
+      this.prevElement.innerText = `${this.getDisplayNumber(this._prevValue)} ${
+        this._operation
+      }`
+    } else {
+      this.prevElement.innerText = ""
+    }
+  }
+
+  chooseOperation(operation) {
+    if(this._currentValue === "" || isNaN(this._currentValue)) return
+    this._prevValue = this._currentValue
+    this._currentValue = ""
+    this._operation = operation
+  }
+}
+
+const numberBtns = document.querySelectorAll("[data-number]")
+const operationBtns = document.querySelectorAll("[data-operation]")
+const prev = document.querySelector(".prev")
+const current = document.querySelector(".current")
+const equalsBtn = document.querySelector(".result")
+
+const calculator = new Calculator(prev, current)
+
+numberBtns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const number = e.target.dataset.value
+    calculator.appendNumber(number)
+    calculator.updateDisplay()
+  })
+})
+
+operationBtns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const operation = e.target.dataset.value
+    calculator.chooseOperation(operation)
+    calculator.updateDisplay()
+  })
+})
+
+equalsBtn.addEventListener("click", () => {
+
+})
